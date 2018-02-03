6e965eeaaddae18e184b8bb66ca1a77a3aca8af0
Mukul <mukul1904@gmail.com>
2018-02-03 17:56:21 +0530

    WIP

--- bx-recommender.py
+++ bx-recommender.py
@@ -7,9 +7,10 @@ import pandas as pd
 # with open('./BX-Datasets-dump/BX-Book-Ratings.csv') as f:
 #     all_lines = f.readlines()
 
-df = pd.read_csv("./BX-Datasets-dump/BX-Book-Ratings.csv", sep=';', encoding = "ISO-8859-1")
+df = pd.read_csv("./BX-Datasets-dump/abc.csv", sep=';')
+# df = pd.read_csv("./BX-Datasets-dump/BX-Book-Ratings.csv", sep=';', encoding = "ISO-8859-1")
 # Prepare the data to be used in Surprise
-reader = Reader(line_format='user item rating', sep=';', rating_scale=(0, 10))
+reader = Reader(line_format='user item rating', sep=';', rating_scale=(1, 5))
 data = Dataset.load_from_df(df, reader)
 
 # Split the dataset into 5 folds and choose the algorithm
@@ -24,6 +25,6 @@ trainset = data.build_full_trainset()
 algo.fit(trainset)
 
 # Predict a certain item
-userid = str("276746")
-itemid = str("0449006522")
+userid = str("6516")
+itemid = str("0515090506")
 print(algo.predict(userid, itemid))
