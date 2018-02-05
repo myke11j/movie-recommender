dd8f7f061f65eac54dba037b8af1358d45e2e090
Mukul <mukul1904@gmail.com>
2018-02-05 18:54:18 +0530

    WIP

--- lightfm-poc.py
+++ lightfm-poc.py
@@ -1,57 +1,38 @@
 import numpy as np
 from lightfm.datasets import fetch_movielens
 from lightfm import LightFM
+import sys
 
-#CHALLENGE part 1 of 3 - write your own fetch and format method for a different recommendation
-#dataset. Here a good few https://gist.github.com/entaroadun/1653794 
-#And take a look at the fetch_movielens method to see what it's doing 
-#
+dataset = fetch_movielens(min_rating=4.0)
 
-#fetch data and format it
-data = fetch_movielens(min_rating=4.0)
+#create & train model
+model = LightFM(loss='bpr') # logistic, warp, bpr, warp-kos
+model.fit(dataset['train'], epochs=30, num_threads=2)
 
-#print training and testing data
-# print(repr(data['train']))
-# print(repr(data['test']))
-
-#CHALLENGE part 2 of 3 - use 3 different loss functions (so 3 different models), compare results, print results for
-#the best one. - Available loss functions are warp, logistic, bpr, and warp-kos.
-
-#create model
-model = LightFM(loss='warp')
-#train model
-model.fit(data['train'], epochs=30, num_threads=2)
-
-
-#CHALLENGE part 3 of 3 - Modify this function so that it parses your dataset correctly to retrieve
-#the necessary variables (products, songs, tv shows, etc.)
-#then print out the recommended results 
 
 def sample_recommendation(model, data, user_ids):
-
-    #number of users and movies in training data
     n_users, n_items = data['train'].shape
-    #generate recommendations for each user we input
     for user_id in user_ids:
 
-        #movies they already like
         known_positives = data['item_labels'][data['train'].tocsr()[user_id].indices]
-        #movies our model predicts they will like
         scores = model.predict(user_id, np.arange(n_items))
-        print(np.argsort(-scores))
-        #rank them in order of most liked to least
+        # print(np.argsort(-scores))
         top_items = data['item_labels'][np.argsort(-scores)]
-
-        #print out the results
-        print("User %s" % user_id)
-        print("     Known positives:")
+        # print(top_items)
+        # print("User ", user_id)
+        print("Movies user %s liked:" % user_id)
 
         for x in known_positives[:3]:
-            print("        %s" % x)
+            print("\t%s" % x)
 
-        print("     Recommended:")
+        print("Movies user %s might like:" % user_id)
 
         for x in top_items[:3]:
-            print("        %s" % x)
+            print("\t%s" % x)
             
-sample_recommendation(model, data, [119])
+users = []
+for id in sys.argv:
+    if sys.argv.index(id) != 0:
+        users.insert(sys.argv.index(id) - 1, id)
+
+sample_recommendation(model, dataset, users) # 219, 118, 36
