44e11fac72e7293d4b9f7146991a00b68d48264e
Mukul <mukul1904@gmail.com>
2019-02-26 11:20:49 +0700

    Add notebook and made changes in recommender python script

--- recommender.py
+++ recommender.py
@@ -1,58 +1,172 @@
+
+# coding: utf-8
+
+# In[24]:
+
+
+# Import necessary libraries
+
 import pandas as pd 
 import numpy as np
 import seaborn as sns
 import warnings
 warnings.filterwarnings('ignore')
 
+print('Pandas version', pd.__version__)
+print('Numpy version', np.__version__)
+print('Seaborn version', sns.__version__)
+
+
+# In[25]:
+
+
 # Read datasets from CSVs
-df = pd.read_csv('datasets/ml-latest-small/ratings.csv', names=['user_id','item_id','rating','titmestamp'])
+df = pd.read_csv('datasets/ml-latest-small/ratings.csv')
 movieTitles = pd.read_csv('datasets/ml-latest-small/movies.csv')
 
-# Drop row at index 0 as it has columns, found out check manually after getting ValueError
-df.drop(df.index[[0]], inplace=True)
 
-# Convert Object to numeric types for processing
-df['item_id'] = df['item_id'].astype(str).astype(int)
-df['rating'] = df['rating'].astype(str).astype(float)
-
-# Renamed movieId column to item_id to merge titles into df
-movieTitles.rename(columns={'movieId':'item_id'}, inplace=True)
-
-df = pd.merge(df, movieTitles, on='item_id')
+# In[26]:
 
 
-# We will create a new dataframe which will have rating for title and number of ratings it received.
+df.head(10)
+
+
+# In[27]:
+
+
+df.info()
+
+
+# In[28]:
+
+
+movieTitles.head(10)
+
+
+# In[29]:
+
+
+movieTitles.info()
+
+
+# In[30]:
+
+
+df = pd.merge(df, movieTitles, on='movieId')
+df.head(10)
+
+
+# We will create a new dataframe which will have rating for title and number of ratings it received.b
+
+# In[57]:
+
+
 averageRatingsDf = pd.DataFrame(df.groupby('title')['rating'].mean())
 averageRatingsDf['numberOfRatings'] = df.groupby('title')['rating'].count()
+averageRatingsDf.head(10)
+
+
+# In[58]:
+
 
 averageRatingsDf = averageRatingsDf.sort_values(['numberOfRatings', 'rating'], ascending=False)
+averageRatingsDf.head(15)
 
-# Some visualization
+
+# In[59]:
+
+
 sns.jointplot(x='rating', y='numberOfRatings', data=averageRatingsDf)
 
+
+# The above scatter plot clarly shows, that avergae rating of movies goes up with more number of ratings.
+
+# In[60]:
+
+
 # Data engineering
-movie_matrix = df.pivot_table(index='user_id', columns='title', values='rating')
+movie_matrix = df.pivot_table(index='userId', columns='title', values='rating')
 movie_matrix.head()
 
-# We will pick two movies to give recommendation
+
+# Now, we will choose two movies, for which we will recommend movies.
+# I've choosen
+# 
+# - Forrest Gump
+# - Braveheart
+
+# In[61]:
+
+
 forest_gump_user_rating = movie_matrix['Forrest Gump (1994)']
 braveheart_user_rating = movie_matrix['Braveheart (1995)']
+
+
+# In[62]:
+
+
+forest_gump_user_rating.head()
+
+
+# In[63]:
+
+
+braveheart_user_rating.head()
+
+
+# In[64]:
+
 
 # Find correlation between watched movies and all movies
 similar_to_forest_gump = movie_matrix.corrwith(forest_gump_user_rating)
 similar_to_braveheart = movie_matrix.corrwith(braveheart_user_rating)
 
+
+# In[65]:
+
+
+similar_to_forest_gump.head()
+
+
+# In[66]:
+
+
+similar_to_braveheart.head()
+
+
+# In[67]:
+
+
 corr_forest_gump = pd.DataFrame(similar_to_forest_gump, columns=['Correlation'])
 corr_forest_gump.dropna(inplace=True)
 corr_forest_gump.head()
+
+
+# In[68]:
+
 
 corr_braveheart = pd.DataFrame(similar_to_braveheart, columns=['correlation'])
 corr_braveheart.dropna(inplace=True)
 corr_braveheart.head()
 
+
+# In[69]:
+
+
 corr_forest_gump = corr_forest_gump.join(averageRatingsDf['numberOfRatings'])
 corr_braveheart = corr_braveheart.join(averageRatingsDf['numberOfRatings'])
 
-#forest_gump_recommended_movies = corr_forest_gump[corr_forest_gump['numberOfRatings'] > 100 ].sort_values(by='Correlation', ascending=False).head(10)
 
-#braveheart_recommended_movies = corr_braveheart[corr_braveheart['numberOfRatings'] > 100 ].sort_values(by='Correlation', ascending=False).head(10)
+# In[72]:
+
+
+rec1 = corr_braveheart[corr_braveheart['numberOfRatings'] > 50 ].sort_values(by='correlation', ascending=False).head(10)
+
+
+# In[74]:
+
+
+rec2 = corr_forest_gump[corr_forest_gump['numberOfRatings'] > 50 ].sort_values(by='Correlation', ascending=False).head(10)
+
+print(rec1);
+print(rec2);
